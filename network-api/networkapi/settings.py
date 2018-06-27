bd95d7098b67d42095f51389df49ecd9a9324a8e
Pomax <pomax@nihongoresources.com>
2018-06-26 17:00:23 -0700

    remove userpermission shims for mezzanine

--- network-api/networkapi/settings.py
+++ network-api/networkapi/settings.py
@@ -222,8 +222,6 @@ if SOCIAL_SIGNIN:
         'social_core.pipeline.social_auth.social_user',
         'social_core.pipeline.user.get_username',
         'social_core.pipeline.user.create_user',
-        # custom permissions when a user logs on
-        'networkapi.utility.userpermissions.set_user_permissions',
         'social_core.pipeline.social_auth.associate_user',
         'social_core.pipeline.social_auth.load_extra_data',
         'social_core.pipeline.user.user_details',
